c208ef252024003477ff677803eeb93f558fdbb9
Alvaro-Osorio <alvaroosorioiba@gmail.com>
2020-04-23 21:43:00 -0500

    29-Post-process-users-processId

---
+++ functions/src/Repositories/User.Repository.ts
@@ -0,0 +1,24 @@
+import {connect} from '../Config/Db';
+import { Repository } from 'typeorm';
+import { User } from '../Models/Entity/Users';
+export class UserRepository{
+    private userRepository:Repository<User>;
+
+    async getConnection(){
+        if(!this.userRepository){
+            this.userRepository = (await connect()).getRepository(User);
+        }
+    }
+
+    async saveUser(user:User){
+        await this.getConnection();
+        return await this.userRepository.save(user);
+    }
+
+    async getUserByName(name:string){
+        await this.getConnection();
+        return await this.userRepository.findOne({
+            where: {name: `${name}`},
+        });
+    }
+}
