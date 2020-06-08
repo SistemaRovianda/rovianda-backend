6ee76d099506bd3da2467e3d361cf1af843cddb6
Jorge <moralesgallegosjorge@gmail.com>
2020-06-08 17:30:42 -0500

    refactor

--- functions/src/Services/Packaging.Service.ts
+++ functions/src/Services/Packaging.Service.ts
@@ -19,14 +19,11 @@ export class PackagingService{
 
     async savePackaging(packagingDTO:PackagingDTO){
 
-        if(!packagingDTO.expiration) throw new Error("[400], expiration is required");
-        if(!packagingDTO.lotId) throw new Error("[400], lotId is required");
-        if(!packagingDTO.observations) throw new Error("[400], observations is required");
-        if(!packagingDTO.packs) throw new Error("[400], packs is required");
-        if(!packagingDTO.pieces) throw new Error("[400], pieces is required");
-        if(!packagingDTO.productId) throw new Error("[400], productId is required");
         if(!packagingDTO.registerDate) throw new Error("[400], registerDate is required");
-        if(!packagingDTO.weight) throw new Error("[400], weight is required");
+        if(!packagingDTO.productId) throw new Error("[400], productId is required");
+        if(!packagingDTO.lotId) throw new Error("[400], lotId is required");
+        if(!packagingDTO.expiration) throw new Error("[400], expiration is required");
+        if(!packagingDTO.products) throw new Error("[400], products is required");
         
         console.log("inicio")
         let product:ProductRovianda = await this.productRoviandaRepository.getProductRoviandaById(packagingDTO.productId);
@@ -36,18 +33,30 @@ export class PackagingService{
         let lot:OvenProducts = await this.ovenRepository.getOvenProductByLot(packagingDTO.lotId);
         if(!lot) throw new Error("[400], lot not found");
 
-        let packaging:Packaging = new Packaging();
-        packaging.expiration = packagingDTO.expiration;
-        packaging.lotId = packagingDTO.lotId;
-        packaging.observations = packagingDTO.observations;
-        packaging.packs = packagingDTO.packs;
-        packaging.pieces = packagingDTO.pieces;
-        packaging.productId = product;
-        packaging.registerDate = packagingDTO.registerDate;
-        packaging.weight = packagingDTO.weight;
+        for( let i =0; i<packagingDTO.products.length; i++){
+            let packaging:Packaging = new Packaging();
+            packaging.registerDate = packagingDTO.registerDate;
+            packaging.productId = product;
+            packaging.lotId = packagingDTO.lotId;
+            packaging.expiration = packagingDTO.expiration;
+            packaging.pieces = packagingDTO.products[i].pieces;
+            packaging.packs = packagingDTO.products[i].packs;
+            packaging.weight = packagingDTO.products[i].weight;
+            packaging.observations = packagingDTO.products[i].observations;
+            await this.packagingRepository.savePackaging(packaging)
+        }
+        // let packaging:Packaging = new Packaging();
+        // packaging.expiration = packagingDTO.expiration;
+        // packaging.lotId = packagingDTO.lotId;
+        // packaging.observations = packagingDTO.observations;
+        // packaging.packs = packagingDTO.packs;
+        // packaging.pieces = packagingDTO.pieces;
+        // packaging.productId = product;
+        // packaging.registerDate = packagingDTO.registerDate;
+        // packaging.weight = packagingDTO.weight;
     
         console.log("hecho")
-        return await this.packagingRepository.savePackaging(packaging);
+        return "registrado";
     }
 
     async getProducts(){
