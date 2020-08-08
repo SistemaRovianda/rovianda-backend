9e498027edf4722c9c7697c29a7b4e720b0529e1
Edgar Castro <m3t4lg34rz@gmail.com>
2020-08-07 19:42:51 -0500

    Ajustes
    Ajustes a reportes de formulacion por fechas

--- functions/src/Repositories/Formulation.Repository.ts
+++ functions/src/Repositories/Formulation.Repository.ts
@@ -52,7 +52,7 @@ export class FormulationRepository{
         await this.getConnection();
         return await this.formulatioRepository.find({
             where: {date: Between(initDate,finDate)},
-            relations: ["formulationIngredients","formulationIngredients.productId"]
+            relations: ["formulationIngredients","formulationIngredients.productId","verifit","make"]
         });
     }
 }
